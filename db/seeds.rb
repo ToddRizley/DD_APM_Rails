9a97ef50f8643253be52fc74b43d3460f20f276b
Todd Rizley <todd.rizley@datadoghq.com>
2017-09-20 22:16:45 -0400

    build small rails app for ddtracer debugging

---
+++ db/seeds.rb
@@ -0,0 +1,18 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
+#
+# Examples:
+#
+#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
+#   Character.create(name: 'Luke', movie: movies.first)
+Animal.create(name:"Henri")
+Animal.create(name:"Philippina")
+Animal.create(name:"Paddington")
+Animal.create(name:"Jean-Phillipe")
+Animal.create(name:"Deb")
+
+Person.create(name:"Mark")
+Person.create(name:"Arnold")
+Person.create(name:"Steff")
+Person.create(name:"Luisa")
+Person.create(name:"Seb")
